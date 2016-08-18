7eb910b6ad3a943dc1d81d4f3ad7779d0c904528
nuancesb <sophcottin@gmail.com>
2016-08-18 15:57:37 +0100

    testing can print a question and receive an answer

--- printing quotes/printingquotes.rb
+++ printing quotes/printingquotes.rb
@@ -1,1 +1,8 @@
+class PrintingQuotes
 
+  def self.execute_program(console,keyboard)
+    quote = keyboard.gets
+    console.print("What is the quote?")
+    console.print("#{quote}")
+  end
+end
--- printing quotes/printingquotes_spec.rb
+++ printing quotes/printingquotes_spec.rb
@@ -1,3 +1,21 @@
 require_relative 'printingquotes.rb'
- 
 
+ describe PrintingQuotes do
+   it "prints a question to the user" do
+     console = StringIO.new("What is the quote?")
+     keyboard = StringIO.new("")
+
+     described_class.execute_program(console,keyboard)
+
+     expect(console.string).to(include("What is the quote?"))
+   end
+
+   it "prints bonjour when a user types it " do
+     console = StringIO.new("What is the quote?")
+     keyboard = StringIO.new("bonjour")
+
+     described_class.execute_program(console,keyboard)
+
+     expect(console.string).to(include("bonjour"))
+   end
+  end
